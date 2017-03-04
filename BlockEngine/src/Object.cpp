9dc73f5ee28be35d2325cb24ae75f513fc119ed6
Eric <Eric@DESKTOP-AR3GH4G>
2017-03-04 13:57:33 -0800

    Added Object class

---
+++ BlockEngine/src/Object.cpp
@@ -0,0 +1,23 @@
+//
+//  Object.cpp
+//  BlockEngine
+//
+//  Created by Eric Schmitt on 3/2/17.
+//  Copyright © 2017 Eric Schmitt. All rights reserved.
+//
+
+#include "Object.hpp"
+
+Object::Object()
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////
+
+Object::~Object()
+{
+    
+}
+
+////////////////////////////////////////////////////////////////////////
---
+++ BlockEngine/src/Object.hpp
@@ -0,0 +1,21 @@
+//
+//  Object.hpp
+//  BlockEngine
+//
+//  Created by Eric Schmitt on 3/2/17.
+//  Copyright © 2017 Eric Schmitt. All rights reserved.
+//
+
+#ifndef Object_hpp
+#define Object_hpp
+
+class Object
+{
+public:
+	Object();
+    ~Object();
+    
+private:
+};
+
+#endif /* Object_hpp */
